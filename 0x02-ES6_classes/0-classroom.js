34a1a1a1be19f0c2271512009775ba47738fd73c
eyzee00 <azizihamza1910@gmail.com>
2024-05-31 01:46:24 +0100

    f

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a classroom.
+ */
+export class Classroom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
